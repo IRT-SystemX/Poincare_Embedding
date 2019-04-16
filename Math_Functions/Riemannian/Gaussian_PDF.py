85ce989a8cfc1a2581fadd4aff5055f2dd525e31
hadi.zaatiti <hadizaatiti@gmail.com>
2019-04-16 17:50:51 +0200

    Gaussian normalizing factor computation.

--- Math_Functions/Riemannian/Gaussian_PDF.py
+++ Math_Functions/Riemannian/Gaussian_PDF.py
@@ -1,22 +1,9 @@
 import math
 from Math_Functions.Riemannian.Distance_Riemannian import *
 
-def Z_D(Sigma):
-
-    return pow((2*math.pi),2/3)*Sigma * math.exp((Sigma*Sigma)/2)*math.erf(Sigma/(math.sqrt(2)))
-
 
 def Normalizing_Factor (Sigma, n):
-
-    C = 1
-
-    product = 1
-    for j in range(1,n-1):
-        product = Z_D(Sigma/(math.sqrt(n-j)))
-
-    Z = C * math.sqrt(2*math.pi/n)*Sigma * product
-
-    return Z
+    return pow((2*math.pi),2/3)*Sigma * math.exp((Sigma*Sigma)/2)*math.erf(Sigma/(math.sqrt(2)))
 
 
 def Gaussian_PDF (X, Mean, Sigma, n):
