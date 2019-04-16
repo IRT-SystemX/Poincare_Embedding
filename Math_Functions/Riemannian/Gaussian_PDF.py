6bbe7f3d814a004b7bbf42a8e39977a1d7e73682
hadi.zaatiti <hadizaatiti@gmail.com>
2019-04-16 13:46:09 +0200

    Gaussian normalizing factor computation.

--- Math_Functions/Riemannian/Gaussian_PDF.py
+++ Math_Functions/Riemannian/Gaussian_PDF.py
@@ -6,11 +6,9 @@ def Z_D(Sigma):
     return pow((2*math.pi),2/3)*Sigma * math.exp((Sigma*Sigma)/2)*math.erf(Sigma/(math.sqrt(2)))
 
 
-
 def Normalizing_Factor (Sigma, n):
 
     C = 1
-    Z_D = pow((2*math.pi),2/3)*Sigma * math.exp((Sigma*Sigma)/2)*math.erf(Sigma/(math.sqrt(2)))
 
     product = 1
     for j in range(1,n-1):
@@ -18,9 +16,21 @@ def Normalizing_Factor (Sigma, n):
 
     Z = C * math.sqrt(2*math.pi/n)*Sigma * product
 
+    return Z
 
 
-def Gaussian_PDF (X, Mean, Sigma,):
+def Gaussian_PDF (X, Mean, Sigma, n):
 
-    Z = 1
+    Z = Normalizing_Factor(Sigma,n)
     return (1/Z)*math.exp(-pow(Riemannian_distance(X, Mean),2))
+
+
+def Gaussian_Mixture (X, Mean, Sigma, n, weights):
+
+    k = len(weights)
+    pdf = 0
+
+    for i in range(k):
+        pdf = pdf + weights[k]*Gaussian_PDF(X,Mean[k], Sigma[k],n)
+
+    return pdf
