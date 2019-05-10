6798242a7b0205e797179a725309d3286af861d5
hadi.zaatiti <hadizaatiti@gmail.com>
2019-05-10 15:11:33 +0200

    visualization mixture

--- Run_EM.py
+++ Run_EM.py
@@ -12,8 +12,11 @@ for i in range(len(Weights)):
 
 Means = np.random.uniform(low=0, high=0.5, size=M) + 1j * np.random.uniform(low=0, high=0.5,
                                                                                   size=M)  # Average means of each gaussian
+
+Means = np.array([0.2+0.2j, -0.2-0.2j, 0.4-0.4j])
+
 #Variances = np.random.uniform(low=0.3, high=0.9, size=M)  # Variances of each gaussian
-Variances = np.array([0.4,0.4,0.4])
+Variances = np.array([0.6,0.6,0.6])
 print('Initial values')
 
 print('\tWeights\n', Weights)
@@ -29,9 +32,9 @@ Z = np.random.uniform(low = 0, high = 0.5, size = size_random)+1j*np.random.unif
 #EM(10, Z)
 
 
-#output_file_name = 'Gaussian_Curve.pdf'
+output_file_name = 'Gaussian_Curve.pdf'
 
-#Plot_Gaussian(Mean, Variance, output_file_name)
+Plot_Gaussian(Means[0], Variances[0], output_file_name)
 
 output_file_name = 'Gaussian_Mixture.pdf'
 
