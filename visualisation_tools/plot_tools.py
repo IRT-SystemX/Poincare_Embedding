4ab7475088714cbae490451f9f0578d8c786286f
tgeral68 <thomas.l.p.gerald@gmail.com>
2019-08-28 18:26:28 +0200

    Adding multiple subplot (each disc) in on figure

--- visualisation_tools/plot_tools.py
+++ visualisation_tools/plot_tools.py
@@ -32,6 +32,61 @@ def weighted_gmm_pdf(w, z, mu, sigma, distance):
 
     return w.unsqueeze(0).expand_as(distribution_normal) * distribution_normal/zeta_sigma.unsqueeze(0).expand_as(distribution_normal) 
 
+
+def subplot_embedding_distribution(ax, W, pi, mu, sigma,  labels=None, N=100, colors=None ):
+    # plotting prior
+    X = np.linspace(-1, 1 ,N)
+    Y = np.linspace(-1, 1 ,N)
+    X, Y = np.meshgrid(X, Y)
+    # plotting circle
+    X0, Y0, radius = 0, 0, 1
+    r = np.sqrt((X - X0)**2 + (Y * Y0)**2)
+    disc = r < 1
+
+    Z = np.zeros((N, N))
+    # compute the mixture 
+    for z_index in range(len(Z)):
+        x =  torch.cat((torch.FloatTensor(X[z_index]).unsqueeze(-1), torch.FloatTensor(Y[z_index]).unsqueeze(-1)), -1)
+        zz = weighted_gmm_pdf(pi, x, mu, sigma, modules.hyperbolicDistance)
+        zz[zz != zz ]= 0
+        Z[z_index] = zz.sum(-1).numpy()
+
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, linewidth=1, antialiased=True, cmap=plt.get_cmap("viridis"))    
+    z_circle = -0.8
+    p = Circle((0, 0), 1, edgecolor='b', lw=1, facecolor='none')
+    ax.add_patch(p)
+    art3d.pathpatch_2d_to_3d(p, z = z_circle, zdir="z")
+
+    for q in range(len(W)):
+        if(colors is not None):
+            ax.scatter(W[q][0].item(), W[q][1].item(), z_circle, c=[colors[q]], marker='.')            
+        else:
+            ax.scatter(W[q][0].item(), W[q][1].item(), z_circle, c='b', marker='.')
+        #print('Print labels', labels[q])
+
+    for j in range(len(mu)):
+        ax.scatter(mu[j][0].item(), mu[j][1].item(), z_circle, c='r', marker='D')
+
+    ax.set_xlim(-1.2, 1.2)
+    ax.set_ylim(-1.2, 1.2)
+    ax.set_zlim(-0.8, 0.4)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('P')
+
+def plot_embedding_distribution_multi(W, pi, mu, sigma, labels=None, N=100, colors=None):
+    fig = plt.figure("Embedding-Distribution")
+    border_size = (math.sqrt(len(W)+0.0))
+    if(border_size != round(border_size)):
+        border_size += 1
+    for i in range(len(W)):
+        ax = fig.add_subplot(border_size, border_size, i+1, projection='3d')
+        subplot_embedding_distribution(ax, W[i], pi[i], mu[i], sigma[i], labels=labels, N=N, colors=colors)
+    plt.savefig("figures/embeddings_distrib_all_disc.pdf", format="pdf")
+
+    return fig
+
 def plot_embedding_distribution(W, pi, mu, sigma,  labels=None, N=100, colors=None):
 
     # TODO : labels colors
