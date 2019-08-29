6ad685d371abd2b4449dffe14b9c027b4b18f0a0
tgeral68 <thomas.l.p.gerald@gmail.com>
2019-08-29 11:00:18 +0200

    Adding a simple Json logger for results

---
+++ launcher_tools/logger.py
@@ -0,0 +1,10 @@
+from data_tools import config
+
+class JSONLogger(config.ConfigurationFile):
+    def __init__(self, filepath, mod="fill"):
+        super(JSONLogger, self).__init__(filepath, mod=mod)
+        self.clear()
+    def append(self, dictionary_like):
+        for k, v in dictionary_like.items():
+            self.__setitem__(k, v)
+    
