472aa63e540b1032c67f08d24f4032b3e23718ec
Jeanne <jeanne@MacBook-Air-de-Jeanne.local>
2019-09-13 12:42:44 +0200

    fix em

--- em_tools/em_original.py
+++ em_tools/em_original.py
@@ -8,7 +8,7 @@ from sklearn.mixture import GaussianMixture
 from sklearn.cluster import KMeans
 from em_tools import kmeans_hyperbolic as kmh
 from function_tools.numpy_function import RiemannianFunction
-
+from visualisation_tools import plot_tools
 
 
 
@@ -56,34 +56,20 @@ class RiemannianTools(object):
 
 
 
-
-
-
-
 class RiemannianEM(object):
     @staticmethod
-    def static_barycenter(z, lr, tau, omega_mu, max_iter=math.inf, g_index=0):
-        N, M = omega_mu.shape
+    def static_barycenter(mu, wik, z, lr, tau, max_iter=math.inf, g_index=0, distance=RiemannianFunction.riemannian_distance):
+        N, M = wik.shape
         cvg = math.inf
-        barycenter = z.mean()
+        barycenter = mu[g_index]
 
         # SGD 
         iteration = 0
         while(cvg>tau and max_iter>iteration):
             iteration+=1
-            
-            mean_nw = RiemannianTools.log(barycenter.repeat(N), z)
-            mean_w = (mean_nw * omega_mu[:, g_index]).mean()
-            if(mean_w.sum() != mean_w.sum()):
-                print("mean->",mean_nw)
-                print(z.shape, barycenter.repeat(N).shape)
-                print("barycenter->",barycenter)
-                print("ERROR NAN Value")
-                print("iteration nb ->",iteration)
-                raise NameError('Not A Number Exception')
-            # update weight step
-            barycenter = RiemannianTools.exp(barycenter, lr * mean_w)
-            cvg = np.sqrt((np.abs(mean_w)**2)/((1 - np.abs(barycenter)**2)**2))
+            grad_value = (RiemannianTools.log(barycenter.repeat(N), z) * wik[:, g_index]).sum()/wik[:, g_index].sum()
+            barycenter = RiemannianTools.exp(barycenter, lr * grad_value)
+            cvg = np.sqrt((np.abs(grad_value)**2)/((1 - np.abs(barycenter)**2)**2))
         return barycenter
 
     # w = M
@@ -95,25 +81,19 @@ class RiemannianEM(object):
         w_pdf = w * pdfs
         return w_pdf/ np.expand_dims(w_pdf.sum(-1),-1).repeat(M,-1)
 
-    # w = M
-    # pdfs = NxM
-    @staticmethod
-    def static_update_w(w, pdfs, g_index=0):
-        return RiemannianEM.static_omega_mu(w, pdfs)[:,g_index].mean()
 
     @staticmethod
-    def static_update_sigma(z, w, mu, pdfs, distance, g_index=0):
-        N, M = pdfs.shape
-        omega_mu = RiemannianEM.static_omega_mu(w, pdfs)
+    def static_update_sigma(z, wik, mu,distance, g_index=0):
+        N, M = wik.shape
         mu = mu[g_index].repeat(N,0)
-        n = (omega_mu[:,g_index] * (distance(z, mu)**2 )).sum(0)
-        return RiemannianFunction.phi(n/omega_mu[:,g_index].sum(0))
+        n = ((wik[:,g_index] * (distance(z, mu)**2) )).sum(0)
+        return RiemannianFunction.phi(n/wik[:,g_index].sum(0))
 
     def __init__(self, n_gaussian, init_mod="rand", verbose=True):
         self._n_g = n_gaussian
         self._distance = RiemannianFunction.riemannian_distance
         self._mu = np.random.uniform(low = -0.5, high = 0.5, size = self._n_g)+1j*np.random.uniform(low = -0.5, high = 0.5, size = self._n_g)
-        self._sigma = np.random.uniform(low = 0.8, high = 0.9, size = self._n_g) 
+        self._sigma = np.random.uniform(low = 0.3, high = 0.4, size = self._n_g) 
         self._w = np.ones(n_gaussian)/n_gaussian
         self._verbose = verbose
         self._init_mod = init_mod
@@ -125,33 +105,24 @@ class RiemannianEM(object):
             print("\t weight -> ", self._w)
         
 
-    def update_w(self, z, g_index=0):
-        self._w[g_index] = RiemannianEM.static_update_w(self._w,
-                                                RiemannianTools.pdf(
-                                                    z, self._mu, 
-                                                    self._sigma, self._distance),
-                                                    g_index=g_index
-                                            )
-    def update_mu(self, z, lr, tau, g_index=0):
-        pdf = RiemannianTools.pdf(z, self._mu, self._sigma, self._distance)
-        omega_mu = RiemannianEM.static_omega_mu(self._w, pdf)
-        self._mu[g_index] = RiemannianEM.static_barycenter(z, lr, tau,
-                                                    omega_mu,
-                                                    max_iter=math.inf,
+    def update_w(self, wik):
+        self._w = wik.mean(0)
+
+    def update_mu(self, z, wik,  lr, tau, g_index=0):
+
+        self._mu[g_index] = RiemannianEM.static_barycenter(self._mu, wik, z, lr, tau,
+                                                    max_iter=200,
                                                     g_index=g_index
                                                 )
 
-    def update_sigma(self, z, g_index):
-        self._sigma[g_index] = RiemannianEM.static_update_sigma(z, self._w, self._mu,
-                                                        RiemannianTools.pdf(
-                                                            z, self._mu, 
-                                                            self._sigma, self._distance
-                                                        ),
+    def update_sigma(self, z, wik,  g_index):
+        self._sigma[g_index] = RiemannianEM.static_update_sigma(z,  wik, self._mu,
                                                        self._distance, g_index=g_index)
 
-    def fit(self, z, max_iter=5, lr_mu=1e-1, tau_mu=5e-3):
+    def fit(self, z, max_iter=5, lr_mu=5e-3, tau_mu=5e-3):
         progress_bar = tqdm.trange(max_iter) if(self._verbose) else range(max_iter)
         # if it is the first time function fit is called
+        z_in = z
         if(not self._started):
             # using kmeans for initializing means
             if(self._init_mod == "kmeans"):
@@ -166,24 +137,30 @@ class RiemannianEM(object):
                 km = kmh.RiemannianKMeans(self._n_g)
                 km.fit(z.numpy())
                 self._mu = km.cluster_centers_[:,0] +km.cluster_centers_[:,1] *1j
-            for g in range(self._n_g): 
-                self.update_sigma(z[:,0].numpy()+z[:,1].numpy() *1j, g_index=g)
             if(self._verbose):
+                print("Weight")
                 print("\t mu -> ", self._mu)
                 print("\t sigma -> ", self._sigma)
             self._started = True
         z = z[:,0].numpy()+z[:,1].numpy() *1j
 
         for epoch in progress_bar:
-            for g in range(self._n_g):
-                self.update_w(z, g_index=g)
-                self.update_mu(z, lr_mu, tau_mu, g_index=g)
-                self.update_sigma(z, g_index=g)
+            wik = self.get_pik(z)
+            self.update_w(wik)
 
+            for g in range(self._n_g):
+                
+                self.update_mu(z, wik, lr_mu, tau_mu, g_index=g)
+                self.update_sigma(z, wik, g_index=g)
+                
     def getParameters(self):
         mu_torch = torch.cat((torch.Tensor(self._mu.real).unsqueeze(-1),torch.Tensor(self._mu.imag).unsqueeze(-1)),-1)
         return  torch.Tensor(self._w), mu_torch, torch.Tensor(self._sigma)
 
+    def get_pik(self, z):
+        return RiemannianEM.static_omega_mu(self._w,RiemannianTools.pdf(
+                                                    z, self._mu, 
+                                                    self._sigma, self._distance))
     def getPik(self, z):
         z = z[:,0].numpy()+z[:,1].numpy() *1j
         return torch.Tensor(RiemannianEM.static_omega_mu(self._w,RiemannianTools.pdf(
