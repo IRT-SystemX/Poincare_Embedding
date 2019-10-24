c80b26f97cce21220ae822b324bbf0170dd139f9
Thomas Gerald <thomas.l.p.gerald@gmail.com>
2019-10-24 15:48:06 +0200

    fix test

--- data_tools/corpora.py
+++ data_tools/corpora.py
@@ -345,14 +345,14 @@ def test_flat_context_corpus():
     start_time = time.time()
     for i, *items in zip(tqdm.trange(len(dataloader_slow)), dataloader_slow):
         x = items[0]
-        g = x + 1
+        g = x[0] + 1
     end_time = time.time()
     print("time to iterate all dataset", end_time-start_time)
     dataloader_fast = dts.RawDataloader(dataset, batch_size=2000)
     start_time = time.time()
     for i, *items in zip(tqdm.trange(len(dataloader_fast)), dataloader_fast):
         x = items[0]
-        g += x+1
+        g = x[0]+1
     end_time = time.time()
     print("time to iterate all dataset", end_time-start_time)
 test_flat_context_corpus()  
